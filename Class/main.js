c9d276531e79872fd86d0fb255c70aea89311bed
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-04-15 21:38:53 -0500

    refactored iteration of object data using OOP

--- Class/main.js
+++ Class/main.js
@@ -1,60 +1,46 @@
-document.querySelector('button').addEventListener('click', getDrink)
+var count = 0;
+var drinkObject;
+window.onload = function() {
+	document.querySelector('button').addEventListener('click', getDrink)
 
-function getDrink(){
-    let drink = document.querySelector('input').value
+	async function getDrink(){
+		let drink = document.querySelector('input').value
+		let data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`).then(res => res.json()).catch(err => {
+			console.log(`error ${err}`)
+		});
+		count = 0;
+		drinkObject = data.drinks.map(drink => {
+			let rObj = {
+				name: drink.strDrink,
+				image: drink.strDrinkThumb,
+				instructions: drink.strInstructions,
+			}
+			return rObj;
+		});
+		document.querySelector('h2').innerText = drinkObject[count].name
+		document.querySelector('img').src = drinkObject[count].image
+		document.querySelector('p').innerText = drinkObject[count].instructions
+	};
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-    .then(res => res.json())
-    .then(data => {
+	document.getElementById('prev').addEventListener('click', previous)
 
-        let drinkNames = [];
-        let drinkInstruct = [];
-        let drinkImage = [];
-        let count = 0;
+	function previous(e){
+		e.preventDefault();
+		count--
+		if (count < 0) count = drinkObject.length - 1;
+		document.querySelector('h2').innerText = drinkObject[count].name
+		document.querySelector('img').src = drinkObject[count].image
+		document.querySelector('p').innerText = drinkObject[count].instructions
+	};
 
-        for(let i = 0; i < data.drinks.length; i++){
-            drinkNames.push(data.drinks[i].strDrink)
-        }
+	document.getElementById('next').addEventListener('click', next)
 
-        for(let i = 0; i < data.drinks.length; i++){
-            drinkImage.push(data.drinks[i].strDrinkThumb)
-        }
-
-        for(let i = 0; i < data.drinks.length; i++){
-            drinkInstruct.push(data.drinks[i].strInstructions)
-        }
-
-        document.querySelector('#prev').addEventListener('click', previous)
-
-        function previous(){
-            if(count < 0){
-                count = drinkNames.length - 1
-            }
-            document.querySelector('h2').innerText = drinkNames[count]
-            document.querySelector('img').src = drinkImage[count]
-            document.querySelector('p').innerText = drinkInstruct[count]
-            count--
-        }
-
-        document.querySelector('#next').addEventListener('click', next)
-
-        function next(){
-            if(count > drinkNames.length - 1){
-                count = 0
-            }
-            document.querySelector('h2').innerText = drinkNames[count]
-            document.querySelector('img').src = drinkImage[count]
-            document.querySelector('p').innerText = drinkInstruct[count]
-            count++
-        }
-
-        document.querySelector('h2').innerText = drinkNames[count]
-        document.querySelector('img').src = drinkImage[count]
-        document.querySelector('p').innerText = drinkInstruct[count]
-
-    })
-
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+	function next(e){
+		e.preventDefault();
+		count++
+		if (count > drinkObject.length - 1) count = 0;
+		document.querySelector('h2').innerText = drinkObject[count].name
+		document.querySelector('img').src = drinkObject[count].image
+		document.querySelector('p').innerText = drinkObject[count].instructions
+	};
 };
